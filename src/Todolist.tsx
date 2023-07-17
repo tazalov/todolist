4cfeb92a920b9c32458a9b11d205b50f5be3e9fd
Iliya Tazalov <134263398+tazalov@users.noreply.github.com>
2023-07-17 22:02:14 +0300

    Sprint 01, 1 lesson

---
+++ src/Todolist.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+type Props = {
+  title: string;
+  tasks: Task[];
+};
+
+type Task = {
+  id: number;
+  title: string;
+  isDone: boolean;
+};
+
+export const Todolist = (props: Props) => {
+  return (
+    <div>
+      <h3>{props.title}</h3>
+      <div>
+        <input />
+        <button>+</button>
+      </div>
+      <ul>
+        {props.tasks.map((task) => (
+          <li key={task.id}>
+            <input type="checkbox" checked={task.isDone} />
+            <span>{task.title}</span>
+          </li>
+        ))}
+      </ul>
+      <div>
+        <button>All</button>
+        <button>Active</button>
+        <button>Completed</button>
+      </div>
+    </div>
+  );
+};
+
+export default Todolist;
