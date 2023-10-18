c11b2861aae7e293d77ba7922153be7a2b632e2a
tazalov <tazalov9@gmail.com>
2023-10-18 16:53:33 +0300

    previous commit is incomplete! create: fetchTasks, fix: jest config

---
+++ src/entities/task/model/services/fetchTasksByTodolistId.ts
@@ -0,0 +1,10 @@
+import { AppThunk } from 'app/providers/store/config/store'
+import { SetTasks } from '../actions/tasks.actions'
+
+export const fetchTasksByTodolistId =
+  (todoListId: string): AppThunk =>
+  async (dispatch, _, extra) => {
+    const { tasksAPI } = extra
+    const response = await tasksAPI.getTasks(todoListId)
+    dispatch(SetTasks(todoListId, response.data.items))
+  }
