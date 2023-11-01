fa28ab6634dfe41cdb8590ebc78d3cd19c54509c
tazalov <tazalov9@gmail.com>
2023-11-01 22:31:28 +0300

    fix: StoreDecorator (add async reducers)

--- src/app/providers/store/ui/StoreProvider.tsx
+++ src/app/providers/store/ui/StoreProvider.tsx
@@ -1,3 +1,4 @@
+import { ReducersMapObject } from '@reduxjs/toolkit'
 import { FC, ReactNode } from 'react'
 import { Provider } from 'react-redux'
 
@@ -9,9 +10,10 @@ import { createReduxStore } from '../config/store'
 type StoreProviderPT = {
   children: ReactNode
   initialState?: DeepPartial<StateSchema>
+  asyncReducers?: ReducersMapObject
 }
 
-export const StoreProvider: FC<StoreProviderPT> = ({ children, initialState }) => {
-  const store = createReduxStore(initialState as StateSchema)
+export const StoreProvider: FC<StoreProviderPT> = ({ children, initialState, asyncReducers }) => {
+  const store = createReduxStore(initialState as StateSchema, asyncReducers)
   return <Provider store={store}>{children}</Provider>
 }
--- src/shared/config/storybook/decorators/StoreDecorator.tsx
+++ src/shared/config/storybook/decorators/StoreDecorator.tsx
@@ -1,109 +1,17 @@
 import { StoryFn } from '@storybook/react'
-
 import { DeepPartial } from 'redux'
 
 import { StateSchema, StoreProvider } from 'app/providers/store'
-import { TaskStatus, TaskPriority } from 'entities/task'
+import { taskReducer } from 'entities/task'
+import { todoListReducer } from 'entities/todolist'
 
-const state: StateSchema = {
-  auth: {
-    data: null,
-    captcha: null,
-    _inited: true,
-  },
-  notification: {
-    status: 'idle',
-    error: null,
-  },
-  todoList: [
-    { id: '1', filter: 'all', title: 'new todolist1', order: 0, addedDate: new Date(), entityStatus: 'idle' },
-    {
-      id: '2',
-      filter: 'all',
-      title: 'new todolist2',
-      order: 1,
-      addedDate: new Date(),
-      entityStatus: 'loading',
-    },
-    { id: '3', filter: 'all', title: 'new todolist3', order: 2, addedDate: new Date(), entityStatus: 'idle' },
-  ],
-  tasks: {
-    '1': [
-      {
-        id: '1',
-        title: 'new task1',
-        status: TaskStatus.NEW,
-        startDate: new Date(),
-        todoListId: '1',
-        order: 0,
-        priority: TaskPriority.LOW,
-        description: '',
-        deadline: new Date(),
-        addedDate: new Date(),
-        entityStatus: 'idle',
-      },
-      {
-        id: '2',
-        title: 'new task2',
-        status: TaskStatus.IN_PROGRESS,
-        startDate: new Date(),
-        todoListId: '1',
-        order: 0,
-        priority: TaskPriority.URGENTLY,
-        description: '',
-        deadline: new Date(),
-        addedDate: new Date(),
-        entityStatus: 'idle',
-      },
-      {
-        id: '3',
-        title: 'new task3',
-        status: TaskStatus.COMPLETED,
-        startDate: new Date(),
-        todoListId: '1',
-        order: 0,
-        priority: TaskPriority.HIGH,
-        description: '',
-        deadline: new Date(),
-        addedDate: new Date(),
-        entityStatus: 'idle',
-      },
-    ],
-    '2': [
-      {
-        id: '1',
-        title: 'new task1',
-        status: TaskStatus.IN_PROGRESS,
-        startDate: new Date(),
-        todoListId: '2',
-        order: 0,
-        priority: TaskPriority.MIDDLE,
-        description: '',
-        deadline: new Date(),
-        addedDate: new Date(),
-        entityStatus: 'idle',
-      },
-    ],
-    '3': [
-      {
-        id: '1',
-        title: 'new task1',
-        status: TaskStatus.COMPLETED,
-        startDate: new Date(),
-        todoListId: '3',
-        order: 0,
-        priority: TaskPriority.HIGH,
-        description: '',
-        deadline: new Date(),
-        addedDate: new Date(),
-        entityStatus: 'idle',
-      },
-    ],
-  },
+const asyncReducers = {
+  tasks: taskReducer,
+  todoList: todoListReducer,
 }
 
 export const StoreDecorator = (initialState?: DeepPartial<StateSchema>) => (Story: StoryFn) => (
-  <StoreProvider initialState={initialState}>
+  <StoreProvider initialState={initialState} asyncReducers={asyncReducers}>
     <Story />
   </StoreProvider>
 )
